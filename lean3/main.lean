8bfa55f24dac9d17eada89808757837c51e7a0b1
Eric Wieser <wieser.eric@gmail.com>
2023-09-01 19:35:59 +0000

    Add an errata from the paper

--- lean3/main.lean
+++ lean3/main.lean
@@ -144,3 +144,57 @@ namespace nested
     module.smul (add_group.neg r) r' = add_group.neg (module.smul r r') := neg_smul r r'
 
 end nested
+
+/-! Unfortunately figure 4 in the paper is incorrect; this demonstrates that the highlighted square
+commutes after all.
+
+This is a counterexample to the claim in
+https://leanprover.zulipchat.com/#narrow/stream/287929-mathlib4/topic/Some.20observations.20on.20eta.20experiment/near/355336941
+-/
+namespace cube
+
+  class nu_na_semiring (R : Type)
+  class nu_na_ring (R : Type) := (to_nu_na_semiring : nu_na_semiring R)
+  attribute [instance] nu_na_ring.to_nu_na_semiring
+  class na_semiring (R : Type) := (to_nu_na_semiring : nu_na_semiring R)
+  attribute [instance] na_semiring.to_nu_na_semiring
+  class nu_semiring (R : Type) := (to_nu_na_semiring : nu_na_semiring R)
+  attribute [instance] nu_semiring.to_nu_na_semiring
+
+  class na_ring (R : Type) := (to_na_semiring : na_semiring R)
+  attribute [instance] na_ring.to_na_semiring
+  class nu_ring (R : Type) := (to_nu_semiring : nu_semiring R)
+  attribute [instance] nu_ring.to_nu_semiring
+
+  class semiring (R : Type) := (to_nu_semiring : nu_semiring R)
+  attribute [instance] semiring.to_nu_semiring
+  class ring (R : Type) := (to_semiring : semiring R)
+  attribute [instance] ring.to_semiring
+
+  instance na_ring.to_nu_na_ring {R : Type} [i : na_ring R] : nu_na_ring R :=
+    { ..i.to_na_semiring}
+  instance nu_ring.to_nu_na_ring {R : Type} [i : nu_ring R] : nu_na_ring R :=
+    { ..i.to_nu_semiring}
+  instance semiring.to_na_semiring {R : Type} [i : semiring R] : na_semiring R :=
+    { ..i.to_nu_semiring}
+
+  instance ring.to_nu_ring {R : Type} [i : ring R] : nu_ring R :=
+    { to_nu_semiring := semiring.to_nu_semiring }
+
+  instance ring.to_na_ring {R : Type} [i : ring R] : na_ring R :=
+    { to_na_semiring := semiring.to_na_semiring }
+
+  example {R} [ring R] :
+    @semiring.to_na_semiring _ (@ring.to_semiring R _) = @na_ring.to_na_semiring _ ring.to_na_ring :=
+  rfl -- commutes after all; whoops
+
+  -- for completeness
+  example {R} [ring R] :
+    @semiring.to_nu_semiring _ (@ring.to_semiring R _) = @nu_ring.to_nu_semiring _ ring.to_nu_ring :=
+  rfl
+  example {R} [ring R] :
+    @nu_ring.to_nu_na_ring _ (@ring.to_nu_ring R _) = @na_ring.to_nu_na_ring _ ring.to_na_ring :=
+  rfl
+
+  -- the other three squares commute trivially, so we omit them
+end cube
